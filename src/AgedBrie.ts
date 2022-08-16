541b57c7d4dc4bb779f92da4d96223c1d92b5db4
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-08-16 19:55:28 +0200

    feat: add Aged Brie item class

---
+++ src/AgedBrie.ts
@@ -0,0 +1,18 @@
+import { Item } from '../src/Item';
+
+export class AgedBrie extends Item {
+  constructor(name: string, sellIn: number, quality: number) {
+    super(name, sellIn, quality);
+  }
+
+  updateQuality() {
+    if (this.sellIn < 0) this.quality += 2;
+    else {
+      this.quality += 1;
+    }
+
+    this.sellIn -= 1;
+
+    this.limitQuality();
+  }
+}
