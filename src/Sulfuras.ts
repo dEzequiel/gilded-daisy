57e744c54f4e498b6be10396a15587031e20c053
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-08-16 21:37:37 +0200

    feat: add Sulfuras item class

---
+++ src/Sulfuras.ts
@@ -0,0 +1,12 @@
+import {Item} from "../src/Item";
+
+export class Sulfuras extends Item {
+    constructor(name: string, sellIn: number) {
+        super(name, sellIn, 80);
+    }
+
+    updateQuality() {
+        this.sellIn = this.sellIn
+        this.quality = this.quality
+    }
+}
