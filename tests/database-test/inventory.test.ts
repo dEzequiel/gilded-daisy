3a7c48cd242dd2113f8920bf0036c893aa9452be
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-08-25 17:16:47 +0200

    feat: prisma setup

---
+++ tests/database-test/inventory.test.ts
@@ -0,0 +1,73 @@
+import {prismaMock} from "../../src/database/singleton";
+import * as db_inventory from "../../src/database/init"
+
+describe('testing sqlite database via prisma orm', () => {
+    test('should create new item', async () => {
+        const item = {
+            id: 1,
+            name: 'Aged Brie',
+            sell_in: 1,
+            quality: 1
+        }
+
+        prismaMock.inventory.create.mockResolvedValue(item)
+
+        await expect(db_inventory.addItem(item)).resolves.toEqual({
+            id: 1,
+            name: 'Aged Brie',
+            sell_in: 1,
+            quality: 1
+        })
+    });
+
+    test('should get all inventory', async () => {
+        const item = [{
+            id: 1,
+            name: 'Aged Brie',
+            sell_in: 1,
+            quality: 1
+        },
+            {
+                id: 2,
+                name: 'Sulfuras',
+                sell_in: 1,
+                quality: 1
+            }]
+
+        prismaMock.inventory.findMany.mockResolvedValue(item)
+
+
+        await expect(db_inventory.getAll()).resolves.toEqual([{
+            id: 1,
+            name: 'Aged Brie',
+            sell_in: 1,
+            quality: 1
+        },
+            {
+                id: 2,
+                name: 'Sulfuras',
+                sell_in: 1,
+                quality: 1
+            }])
+    })
+
+    test('should delete an item', async () => {
+        const item = {
+            id: 1,
+            name: 'Aged Brie',
+            sell_in: 1,
+            quality: 1
+        }
+
+        prismaMock.inventory.delete.mockResolvedValue(item)
+
+
+        await expect(db_inventory.deleteItem(1)).resolves.toEqual({
+            id: 1,
+            name: 'Aged Brie',
+            sell_in: 1,
+            quality: 1
+        })
+    })
+
+})
